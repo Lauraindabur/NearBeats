91b9a8dacf2c930ce68abfc6b96a2fceb1c785de
GITlaura <lalaindaburgarcia@gmail.com>
2025-08-12 00:57:53 -0500

    Elimino todo el contenido para evitar conflictos

--- main/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Song
-# Register your models here.
-admin.site.register(Song)  # Register the Song model to make it available in the admin interface

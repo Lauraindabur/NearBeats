8f42ed3274a19c0bf9dc63aec1296b5f52705c83
Daniela Salazar Amaya <dsalazara1@eafit.edu.co>
2025-08-12 13:12:13 -0500

    home.html pueda obtener los elementos filtrados e imprimirlos en texto plano

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Song
 
 # Register your models here.
+
+#Para poder garantizar quee el modelo se incluya en la interfaz de administración
+admin.site.register(Song)
+
+#Quede en punto 7
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse    
+from django.http import HttpResponse  
+from .models import Song
+from random import randint
 
 # Create your views here.
 
@@ -11,4 +13,38 @@ def home(request):
     return render(request, "main/home.html")  # Asegúrate de que el archivo home.html existe en la carpeta templates/main
 
 def filtrar_sugerencias(request):
-    pass
+
+    emotion = request.GET.get("emocion")
+    created_at = request.GET.get("created_at")
+    genre = request.GET.get("genre")
+    random = request.GET.get("random")
+
+    songs = Song.objects.all()
+
+    if(random):
+        # Obtenemos elemento en posición aleatoria
+        count = Song.objects.count()
+        if ( count > 0):
+            random_index = randint(0, count-1)
+            songs=Song.objects.all()[random_index]
+
+    else:
+        if emotion:
+            songs= songs.filter(mood=emotion)
+        if created_at:
+            songs = songs.filter(created_at=created_at)
+        if genre:
+            songs = songs.filter(genre=genre)
+    
+    #Retornamos el diccionario con los valores
+    return render(request, "home.html", {"songs": songs})
+    # if (request.GET.get("random") == 1):
+    #     pass
+    # else:
+    #     # Obtenemos parametros mandados en la URL: /home/?emocion=sad&created_at=2000&genre=pop
+    #     emotion = request.GET.get("emocion")
+    #     created_at = request.GET.get("created_at")
+    #     genre = request.GET.get("genre")
+
+    #     # Obtenemos todos los datos de la bd
+    #     songs = Song.objects.filter(emotion, created_at, genre)

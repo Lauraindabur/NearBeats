ccb22f96ca7054b08d027d548adea3e0c5be4bbb
Sahian Salome <sahiangutierrez14@gmail.com>
2025-08-13 21:21:55 -0500

    Funcion subir canciones

--- NearBeats/urls.py
+++ NearBeats/urls.py
@@ -24,6 +24,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', musicViews.library, name="library"),
+    path('upload/', musicViews.upload_songs, name='upload_songs'),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
